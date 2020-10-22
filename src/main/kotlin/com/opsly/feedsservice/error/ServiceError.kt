d017f194a2aca5f74a12054d6a1bc5629c20bdc0
nikolaos chri <nick.christidis@yahoo.com>
2020-10-22 21:03:52 +0300

    remove unused adts error

--- src/main/kotlin/com/opsly/feedsservice/error/ServiceError.kt
+++
@@ -1,6 +0,0 @@
-package com.opsly.feedsservice.error
-
-sealed class ServiceError
-object FeedProviderTimeoutError : Error()
-object FeedProviderMalformedResponseError : Error()
-object FeedProviderNotReachableError : Error()
